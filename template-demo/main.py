236f148876382fd2999fefc8e3b5fc01d1591827
Vakkalanka Sai Shashank <vsaishashank10@gmail.com>
2026-07-23 18:02:21 +0530

    Update main.py

--- template-demo/main.py
+++ template-demo/main.py
@@ -11,8 +11,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Initialize Jinja2 templates
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/messesges", response_class=HTMLResponse)
 async def read_root(request: Request):
     # Context data passed to the template
     context = {"message": "Hello, World!"}
-    return templates.TemplateResponse("index.html", {"request": request, **context})   
+    return templates.TemplateResponse("index.html", {"request": request, **context})
+
+@app.get("/messeges1/v1/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    # Context data passed to the template
+    context = {"message": "Hello, World!"}
+    return templates.TemplateResponse("index.html", {"request": request, **context})
+
+print("Hello World");
+SELECT * FROM EMP;
